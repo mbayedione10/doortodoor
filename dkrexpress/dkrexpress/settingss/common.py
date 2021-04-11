abe73378acd25a1cd082aaa4e920d568872bc104
mbayedione10 <mbayedione10@gmail.com>
2021-04-11 17:25:22 +0000

    settings to deploy on Google Cloud Platform

--- dkrexpress/dkrexpress/settingss/common.py
+++ dkrexpress/dkrexpress/settingss/common.py
@@ -94,12 +94,45 @@ WSGI_APPLICATION = 'dkrexpress.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.11/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#     }
+# }
+
+
+# [START db_setup]
+if os.getenv('GAE_APPLICATION', None):
+    # Running on production App Engine, so connect to Google Cloud SQL using
+    # the unix socket at /cloudsql/<your-cloudsql-connection string>
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.mysql',
+            'HOST': '/cloudsql/doortodoor-310209:us-central1:doortodoor-instance',
+            'USER': 'nioulboy',
+            'PASSWORD': 'nioulboy',
+            'NAME': 'doortodoor',
+        }
     }
-}
+else:
+    # Running locally so connect to either a local MySQL instance or connect 
+    # to Cloud SQL via the proxy.  To start the proxy via command line: 
+    #    $ cloud_sql_proxy -instances=[INSTANCE_CONNECTION_NAME]=tcp:3306 
+    # See https://cloud.google.com/sql/docs/mysql-connect-proxy
+    DATABASES = {
+            'default': {
+                'ENGINE': 'django.db.backends.mysql',
+                'HOST': '127.0.0.1',
+                'PORT': '3306',
+                'NAME': 'doortodoor',
+                'USER': 'nioulboy',
+                'PASSWORD': 'nioulboy',
+            }
+    }
+
+# [END db_setup]
+
 
 
 # Password validation
