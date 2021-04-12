ff5b69c6a97fc9042346644ad52f65bf1ba02a9a
mbayedione10 <mbayedione10@gmail.com>
2021-04-12 15:21:37 +0000

    Production settings

---
+++ dkrexpress/dkrexpress/settingss/production.py
@@ -0,0 +1,20 @@
+from dkrexpress.settingss.common import *
+
+
+DEBUG = False
+
+ALLOWED_HOSTS = [
+    '*'
+]
+
+
+
+# #HTTPS Settings
+# SESSION_COOKIE_SECURE = True
+# CSRF_COOKIE_SECURE = True
+# SECURE_SSL_REDIRECT = True
+
+# #HSTS settings
+# SECURE_HSTS_SECONDS = 31536000 #1 year
+# SECURE_HSTS_PRELOAD = True
+# SECURE_HSTS_INCLUDE_SUBDOMAINS = True
