a30b418f5f1ddec9b9405cffc9eac018bdda44b3
mbayedione10 <mbayedione10@gmail.com>
2021-12-24 16:58:17 +0000

    style correction for models

--- dkrexpress/doortodoor/models.py
+++ dkrexpress/doortodoor/models.py
@@ -1,24 +1,26 @@
 from django.db import models
 from django.contrib.auth.models import User
+
 # Create your models here.
 
 EN_COURS = 'en cours'
 LIVRE = 'livré'
 RETOUR = 'retour'
 
-STATUS_TYPES=(
-    (EN_COURS,EN_COURS),
-    (LIVRE,LIVRE),
-    (RETOUR,RETOUR),
+STATUS_TYPES = (
+    (EN_COURS, EN_COURS),
+    (LIVRE, LIVRE),
+    (RETOUR, RETOUR),
 )
+
 
 class Article(models.Model):
     libelle = models.CharField(max_length=100)
-    #description =  models.TextField(blank=True)
+    # description =  models.TextField(blank=True)
     prix_article = models.IntegerField(blank=True)
-    montant_livraison =  models.IntegerField(blank=True)
+    montant_livraison = models.IntegerField(blank=True)
     user = models.ManyToManyField(User)
-    #Information du client
+    # Information du client
     nom_client = models.CharField(max_length=100)
     adresse_client = models.CharField(max_length=150, null=False)
     contact_client = models.PositiveIntegerField(null=False)
@@ -32,15 +34,11 @@ class Article(models.Model):
 class Livraison(models.Model):
     article = models.ManyToManyField('Article', related_name='article')
     user = models.ManyToManyField(User)
-    statut = models.CharField(max_length=20, choices=STATUS_TYPES, blank = False, null=False,  default=EN_COURS)
+    statut = models.CharField(max_length=20, choices=STATUS_TYPES, blank=False, null=False, default=EN_COURS)
     date_statut = models.DateTimeField(auto_now_add=True)
-    prix_livraison = models.IntegerField(default=0,blank=True)
+    prix_livraison = models.IntegerField(default=0, blank=True)
     created_on = models.DateTimeField(auto_now_add=True)
-    
+
     def __str__(self):
         return f'Livraison : {self.statut}'
         # date_statut.strftime("%b %d %Y %I:%M %p")}'
-
-
-
-
